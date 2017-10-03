ad5dcc32fd0cc81e4ccdff1cbc79312ddd0a7b28
Shobhit Maheshwari <shobhit20897@gmail.com>
2017-10-03 22:52:07 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 import SceneDesc
 
-sd = SceneDesc.scenedesc()
-model = sd.create_model()
-batch_size = 512
-model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=30, verbose=2, callbacks=None)
-model.save('output/Model.h5', overwrite=True)
-model.save_weights('output/Weights.h5',overwrite=True)
+
+def train():
+    sd = SceneDesc.scenedesc()
+    model = sd.create_model()
+    batch_size = 512
+    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=30, verbose=2, callbacks=None)
+    model.save('Output/Model.h5', overwrite=True)
+    model.save_weights('Output/Weights.h5',overwrite=True)
+   
+if __name__=="__main__":
+    train()
