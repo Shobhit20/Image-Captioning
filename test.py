0a43f9e37fe57e4ecc7d451477cca622ed8836d6
Shobhit Maheshwari <shobhit20897@gmail.com>
2017-12-09 15:16:37 -0600

    with image as sys arg

---
+++ test.py
@@ -0,0 +1,26 @@
+import encode_image as ei
+import SceneDesc
+import test_mod as tm
+import time as time
+import pyttsx
+
+def text(img):
+	t1= time.time()
+	encode = ei.model_gen()
+	weight = 'output/Weights.h5'
+	sd = SceneDesc.scenedesc()
+	model = sd.create_model(ret_model = True)
+	model.load_weights(weight)
+	image_path = img
+	encoded_images = ei.encodings(encode, image_path)
+
+	image_captions = tm.generate_captions(sd, model, encoded_images, beam_size=3)
+	engine = pyttsx.init()
+	engine.say(	str(image_captions))
+	engine.runAndWait()
+
+
+if __name__ == '__main__':
+	image = raw_input("Enter the name of the input image")
+	image = "test/"+image
+	text(image)
