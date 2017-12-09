41a2b00a4a7f1245e31329597d70d65ba86dd35d
Shobhit Maheshwari <shobhit20897@gmail.com>
2017-12-10 02:49:03 +0530

    added epochs as sys arg

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 import SceneDesc
 
+import sys
 
-def train():
+def train(epoch):
     sd = SceneDesc.scenedesc()
     model = sd.create_model()
     batch_size = 512
-    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=30, verbose=2, callbacks=None)
+    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=None)
     model.save('Output/Model.h5', overwrite=True)
     model.save_weights('Output/Weights.h5',overwrite=True)
-   
+ 
 if __name__=="__main__":
-    train()
+    train(int(sys.argv[1]))
